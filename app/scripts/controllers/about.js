28aa19501e1d63380ab8a15041b7fb072d53cda2
el-sonny <sonny@spaceshiplabs.com>
2016-01-15 13:02:05 -0500

    yo angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name salamaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the salamaApp
+ */
+angular.module('salamaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name salamaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the salamaApp
+ */
+angular.module('salamaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
