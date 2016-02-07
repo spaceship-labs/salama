3ee400b5c5b8b15d8ed47ae0fa9e91e354c67c50
tugorez <tugorez@gmail.com>
2016-02-06 18:49:37 -0600

    added dynamic directive and respective templates skeletons

---
+++ app/scripts/directives/form-question.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name salamaApp.directive:formQuestion
+ * @description
+ * # formQuestion
+ */
+(function(){
+  angular.module('salamaApp')
+    .directive('formQuestion',formQuestion);
+
+  function formQuestion(){
+    var template = '<ng-include src="\'views/questions/\'+question.type+\'.html\'"></ng-include>';
+    return {
+      template: template,
+      scope: {
+        question: '=',
+      }
+    };
+  }
+})();
+
