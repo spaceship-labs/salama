2a4f959e0c02aded6b099817d4826cc14031115f
Angel Roca <angelroca891020@gmail.com>
2016-03-07 19:55:57 -0500

    sidenav close clicking outside

--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -7,28 +7,102 @@
  * # ContratoCtrl
  * Controller of the ctbookApp
  */
+
 (function(){
   angular
     .module('salamaApp')
     .controller('HeaderCtrl', HeaderCtrl);
 
-  HeaderCtrl.$inject=['$translate', 'navsideService'];
+  HeaderCtrl.$inject=['$scope', '$translate', 'metadataService', 'postsService','$mdSidenav', '$window', '$timeout'];
 
-  function HeaderCtrl($translate, navsideService){
+  function HeaderCtrl($scope, $translate, metadataService, postsService, $mdSidenav, $window, $timeout){
 
     var ctrl = this;
-
+    // headerStuff
     ctrl.showLangOptions = false;
     ctrl.changeLang = changeLang;
     ctrl.changeStateSide = changeStateSide;
+    // navsideStuff
+    ctrl.show = false;
+    ctrl.categories = [];
+    ctrl.metadata = {};
+    ctrl.changeStateSide = changeStateSide;
+    ctrl.setPost = setPost;
+    ctrl.headerFixed = false;
 
+    activate();
+
+    // newFoo
+    function changeStateSide(){
+      $mdSidenav('left').toggle();
+    }
+
+    function setFixedMenu(){
+      angular.element($window).bind('scroll',function (){
+        if($window.pageYOffset > 60){
+          $timeout(function (){
+            ctrl.headerFixed = true;
+          });
+        }else{
+          $timeout(function (){
+            ctrl.headerFixed = false;
+          });
+        }
+      });
+    }
+
+    // headerFoo
     function changeLang(lang){
       ctrl.showLangOptions = !ctrl.showLangOptions;
       $translate.use(lang);
     }
 
-    function changeStateSide(){
-      navsideService.changeState();
+    // navsideFoo
+
+    function activate(){
+      // $scope.$watch(getState, setState);
+      $scope.$watch(getLang, getMetadata);
+      setFixedMenu();
+    }
+
+    function setState(show){
+      ctrl.show = show;
+    }
+
+    function getLang(){
+      return $translate.use();
+    }
+
+    function getMetadata(lang){
+      return metadataService.getMetadata(lang)
+      .then(setCategories)
+      .then(setMetadata);
+    }
+
+    function setCategories(metadata){
+      var categories = [];
+      metadata.forEach(function(m){
+        var category = m.category;
+        categories.indexOf(category) != -1 || categories.push(category);
+      });
+      ctrl.categories = categories;
+      return metadata;
+    }
+
+    function setMetadata(repoMetadata){
+      var metadata = {};
+      repoMetadata.forEach(function(m){
+        var category = m.category;
+        metadata[category] = metadata[category] || [];
+        metadata[category].push(m);
+      });
+      ctrl.metadata = metadata;
+      return repoMetadata;
+    }
+
+    function setPost(path){
+      changeStateSide();
+      postsService.setSelected(path);
     }
 
     function logError(err){
