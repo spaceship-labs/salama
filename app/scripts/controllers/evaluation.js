c7e2726c6649e8dce54c0537d3f7c190ca4f73f9
tugorez <tugorez@gmail.com>
2016-03-09 18:45:00 -0600

    added locales's text  in header,navside,home and footer

--- app/scripts/controllers/evaluation.js
+++ app/scripts/controllers/evaluation.js
@@ -27,57 +27,58 @@
     organizationsService
   ){
 
-    var ctrl = this;
     var urlApi = 'http://salama-api.herokuapp.com/survey';
+    var ctrl = this;
+    var risks;
+    ctrl.questions = [];
+    ctrl.answers = {};
     ctrl.individuals = 'individual';
     ctrl.organizations = 'organizations';
     ctrl.type = undefined;
-    ctrl.page = 0 ;
+    ctrl.page = 0;
     ctrl.completed = 0;
-    ctrl.questions = [];
-    ctrl.answers = {};
+    ctrl.checkResults = false;
+    ctrl.finalScore = 0;
     ctrl.setType = setType;
     ctrl.finishEvaluation = finishEvaluation;
-    ctrl.checkResults = false;
-    ctrl.finalScore = 85;
-    ctrl.risks = {
-      'extreme' : {
-        'level'       : 'Extremo',
-        'image'       : '/images/riesgo_extremo.jpg',
-        'description' : 'Suspender actividad si el riesgo es inaceptable y fallan estrategias'
+
+    risks = {
+      extreme : {
+        level       : 'Extremo',
+        image       : '/images/riesgo_extremo.jpg',
+        description : 'Suspender actividad si el riesgo es inaceptable y fallan estrategias'
       },
-      'high'    : {
-        'level'       : 'Alto',
-        'image'       : '/images/riesgo_alto.jpg',
-        'description' : 'Require medidas proactivas y reducción de riesgos inaceptables'
+      high    : {
+        level       : 'Alto',
+        image       : '/images/riesgo_alto.jpg',
+        escription : 'Require medidas proactivas y reducción de riesgos inaceptables'
       },
-      'medium'  : {
-        'level'       : 'Moderado',
-        'image'       : '/images/riesgo_medio.jpg',
-        'description' : 'Estrategias de prevención para mitigar el daño eventual'
+      medium  : {
+        level       : 'Moderado',
+        image       : '/images/riesgo_medio.jpg',
+        description : 'Estrategias de prevención para mitigar el daño eventual'
       },
-      'low'     : {
-        'level'       : 'Bajo',
-        'image'       : '/images/riesgo_bajo.jpg',
-        'description' : 'Continuidad de actividades'
+      low     : {
+        level       : 'Bajo',
+        image       : '/images/riesgo_bajo.jpg',
+        description : 'Continuidad de actividades'
       }
     };
 
     activate();
 
-    ctrl.checkResultsFoo = function (){
-      if(ctrl.finalScore > 90){
-        ctrl.risk = ctrl.risks.low;
-      }else
-      if(ctrl.finalScore > 70){
-        ctrl.risk = ctrl.risks.medium;
-      }else
-      if(ctrl.finalScore > 50){
-        ctrl.risk = ctrl.risks.high;
-      }else{
-        ctrl.risk = ctrl.risks.extreme;
+    function setResults() {
+      if (ctrl.finalScore <= 40) {
+        ctrl.risk = risks.low;
+      } else
+      if (ctrl.finalScore <= 59) {
+        ctrl.risk = risks.medium;
+      } else
+      if (ctrl.finalScore <= 79) {
+        ctrl.risk = risks.high;
+      } else {
+        ctrl.risk = risks.extreme;
       }
-
       ctrl.checkResults = true;
     }
 
@@ -86,7 +87,7 @@
     }
 
     function getLangAndType(){
-        return $translate.use() + ctrl.type;
+      return $translate.use() + ctrl.type;
     }
 
     function getQuestions(){
@@ -115,10 +116,30 @@
     function finishEvaluation(){
       ctrl.completed = 100;
       ctrl.page = 2;
+      setScore();
       sendQuestions();
     }
 
+    function getScore(){
+      var score = 0;
+      ctrl.questions.map(function(page){
+        page.questions.map(function(question){
+          if (question.score) {
+            score += Number(ctrl.answers[question.name]) || 0;
+          }
+        });
+      });
+      return score;
+    }
+
+    function setScore(){
+      var score = getScore();
+      ctrl.finalScore = score || 0;
+      setResults();
+    }
+
     function sendQuestions(){
+      return;
       ctrl.answers['survey_type'] = ctrl.type;
       $http.post(urlApi, ctrl.answers).success(function(a){
         console.log(a);
